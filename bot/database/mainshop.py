1627f590bdf748f35cadbf7e40805e86fa5906e3
Cooldude069 <70008729+Cooldude069@users.noreply.github.com>
2021-03-30 20:31:45 +0530

    added new items!!

--- bot/database/mainshop.py
+++ bot/database/mainshop.py
@@ -1,7 +1,10 @@
 from models.item import Item
 
 shop = [
+    Item("apple", ":apple:", price=50),
     Item("watch", ":stopwatch:", price=100),
+    Item("pizza", ":pizza:", price=300),
     Item("phone", ':mobile_phone:', price=1000),
+    Item("bow", ":bow_and_arrow:", price=5000),
     Item("dragon", ":dragon_face:", price=100000)
 ]
