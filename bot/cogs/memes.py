753d0ef06bbe944f147868ee38925dc747e5856d
Samarth Nasnodkar <70008729+Cooldude069@users.noreply.github.com>
2021-06-25 11:58:31 +0530

    Update memes.py

--- bot/cogs/memes.py
+++ bot/cogs/memes.py
@@ -16,11 +16,11 @@ class Memes(commands.Cog):
         self.client = client
         self.cluster = MongoClient(environ.get('mongo_url'))
         self.subreddits = ['dankmemes', 'memes', 'PrequelMemes', 'pcmasterrace', 'PewdiepieSubmissions']
-        self.reddit = asyncpraw.Reddit(client_id='0bD1UHrRzjDbGQ',
-                                       client_secret='9xoApJv0eZeRr1QVGJJulIE5cjXyFg',
-                                       username='CooLDuDE-6_9',
+        self.reddit = asyncpraw.Reddit(client_id='R_PyTZLL1EMatg',
+                                       client_secret='k_Hj2HG9ZWRE843H1cLF9k5aCfKP7A',
+                                       username='I_BANG_MOMS',
                                        password='samarth1709',
-                                       user_agent='AmongUsUnofficial')
+                                       user_agent='Peeko')
         self.updateMeme.start()
 
     @tasks.loop(minutes=30)
